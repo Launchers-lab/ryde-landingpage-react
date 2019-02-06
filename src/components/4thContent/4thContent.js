0601533fe9ac3d9d1519196dcfc12b908a7592ee
Go Tae Geon <gtg7784@lab-c.me>
2019-02-07 02:06:32 +0900

    시각보정// '탄만큼 내세요' 부분, 가운데 정렬

--- src/components/4thContent/4thContent.js
+++ src/components/4thContent/4thContent.js
@@ -10,26 +10,28 @@ export default class Content4th extends Component {
   render () {
     return (
         <div className={cx('content-4th')}>
-          <span>RYDE, 좀 더 알아볼까요?</span>
-          <p>새로운 새미 모빌리티의 시작, 라이드가 주도합니다.</p>
-          <ul>
-            <li>
-              <div>Lorem Ipsum dolor sit amet</div>
-              <MaterialIcon icon='check' color='#ff4077' size='18' />
-            </li>
-            <li>
-              <div>Lorem Ipsum dolor sit amet</div>
-              <MaterialIcon icon='check' color='#ff4077' size='18' />
-            </li>
-            <li>
-              <div>Lorem Ipsum dolor sit amet</div>
-              <MaterialIcon icon='check' color='#ff4077' size='18' />
-            </li>
-            <li>
-              <div>Lorem Ipsum dolor sit amet</div>
-              <MaterialIcon icon='check' color='#ff4077' size='18' />
-            </li>
-          </ul>
+          <div>
+            <span>RYDE, 좀 더 알아볼까요?</span>
+            <p>새로운 새미 모빌리티의 시작, 라이드가 주도합니다.</p>
+            <ul>
+              <li>
+                <div>Lorem Ipsum dolor sit amet</div>
+                <MaterialIcon icon='check' color='#ff4077' size='18' />
+              </li>
+              <li>
+                <div>Lorem Ipsum dolor sit amet</div>
+                <MaterialIcon icon='check' color='#ff4077' size='18' />
+              </li>
+              <li>
+                <div>Lorem Ipsum dolor sit amet</div>
+                <MaterialIcon icon='check' color='#ff4077' size='18' />
+              </li>
+              <li>
+                <div>Lorem Ipsum dolor sit amet</div>
+                <MaterialIcon icon='check' color='#ff4077' size='18' />
+              </li>
+            </ul>
+          </div>
         </div>
     )
   }
