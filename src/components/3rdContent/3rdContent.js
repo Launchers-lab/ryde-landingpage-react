45433065ceb06c4ebfe413560044f0f30e87f64e
Go Tae Geon <gtg7784@lab-c.me>
2019-02-09 03:24:28 +0900

    3rd 반응형

--- src/components/3rdContent/3rdContent.js
+++ src/components/3rdContent/3rdContent.js
@@ -34,27 +34,35 @@ export default class Content3rd extends Component {
             </div>
             <div className="cards">
               <span>First name</span>
-              <span>CEO</span>
+              <div>CEO</div>
               <p>
                 He comes from the non-acting branch of the Coppola family: his father August (brother of Francis)
               </p>
             </div>
             <div className="cards">
               <span>First name</span>
-              <span>CEO</span>
+              <div>CEO</div>
               <p>
                 He comes from the non-acting branch of the Coppola family: his father August (brother of Francis)
               </p>
             </div>
             <div className="cards">
               <span>First name</span>
-              <span>CEO</span>
+              <div>CEO</div>
               <p>
                 He comes from the non-acting branch of the Coppola family: his father August (brother of Francis)
               </p>
             </div>
             <div></div>
           </div>
+
+
+          <span>
+            이동의 문화를 만듭니다.
+          </span>
+          <p>
+            RYDE와 함께할 새로운 팀원을 <br/> 모집하고 있습니다.
+          </p>
         </div>
     )
   }
