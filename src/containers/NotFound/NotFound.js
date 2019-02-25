8a48b9f41120bfe8b6c9f7f8fd4994165e1a786a
Go Tae Geon <gtg7784@lab-c.me>
2019-02-25 23:31:08 +0900

    add img to 404 page

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as classNames from 'classnames/bind'
 
+import img from '../../assets/404.svg'
 import styles from './NotFound.scss';
 const cx = classNames.bind(styles)
-
-
-
 export default class NotFound extends Component {
   render() {
     return (
       <div className={cx('NotFound')}>
+        <img src={img} alt="404-NOTFOUND"/>
         <h1>404 - Page not found</h1>
         <p>
         죄송합니다. 찾으시는 페이지가 존재하지 않습니다. <br/>
