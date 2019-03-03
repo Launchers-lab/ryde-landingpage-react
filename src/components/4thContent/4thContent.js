cf3eba6e6ab41c671e5c5c08b98d574bdb1d3c07
Go Tae Geon <gtg7784@lab-c.me>
2019-03-04 01:45:33 +0900

    UPDATE 4th content

--- src/components/4thContent/4thContent.js
+++ src/components/4thContent/4thContent.js
@@ -15,21 +15,21 @@ export default class Content4th extends Component {
             <p>새로운 새미 모빌리티의 시작,<br/> 라이드가 주도합니다.</p>
             <ul>
               <li>
-                <div>Lorem Ipsum dolor sit amet</div>
+                <div>집에서 역까지 걸어가기 힘들다고요?</div>
                 <MaterialIcon icon='check' color='#ff4077' size='18' />
               </li>
               <li>
-                <div>Lorem Ipsum dolor sit amet</div>
+                <div>스쿠터를 대여하자니 반납해야되서 귀찮다고요?</div>
                 <MaterialIcon icon='check' color='#ff4077' size='18' />
               </li>
               <li>
-                <div>Lorem Ipsum dolor sit amet</div>
+                <div>택시도 애매하고 걷기도 애매하다고요?</div>
                 <MaterialIcon icon='check' color='#ff4077' size='18' />
               </li>
-              {/* <li>
-                <div>Lorem Ipsum dolor sit amet</div>
+              <li>
+                <div>걷기엔 늦을거같은데 뛰어가긴 싫다고요?</div>
                 <MaterialIcon icon='check' color='#ff4077' size='18' />
-              </li> */}
+              </li>
             </ul>
           </div>
         </div>
