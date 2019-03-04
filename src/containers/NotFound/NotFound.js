9eef58c2b35489f7d4bcbd31aa01e18c960818bd
Go Tae Geon <gtg7784@lab-c.me>
2019-03-05 00:53:56 +0900

    UPDATE 404 onClick href

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import * as classNames from 'classnames/bind'
 
 import img from '../../assets/404.svg'
 import styles from './NotFound.scss';
 const cx = classNames.bind(styles)
 export default class NotFound extends Component {
+  constructor(props){
+    super(props)
+
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onClick(e){
+    console.log('clicked')
+    window.location.href = "/"
+  }
+
   render() {
     return (
       <div className={cx('NotFound')}>
@@ -15,9 +25,8 @@ export default class NotFound extends Component {
         죄송합니다. 찾으시는 페이지가 존재하지 않습니다. <br/>
         URL이 정확한지 다시 한 번 확인해주세요.
         </p>
-        <Link to='/'>
-          <button>메인화면으로 이동</button>
-        </Link>
+
+        <button onClick={this.onClick}>메인화면으로 이동</button>
       </div>
     );
   }
