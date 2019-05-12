c6dc0482369d03da6f0600eec8b8ab59c9fbd8d4
Go Tae Geon <gtg7784@lab-c.me>
2019-05-12 18:27:00 +0900

    remove hot loader

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { hot } from 'react-hot-loader'
 import { Switch, Route } from 'react-router-dom'
 
 import * as classNames from 'classnames/bind'
@@ -28,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default hot(module)(App)
+export default App
